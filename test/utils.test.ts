6341da71458a72276fda65750e5cdefd697fcd99
drewrh <drewrh04@gmail.com>
2023-01-22 17:02:10 -0500

    Updated tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,46 +1,42 @@
-import { generateDomain } from '@/utils'
+import { findConstraint, generateAllDomains, generateDomain } from '@/utils'
 
-describe('Generating domains', () => {
-  test('with no values returns an empty array', () => {
-    expect(generateDomain([0], 1)).toEqual([])
+test('domains generated correctly', () => {
+  const actual = generateAllDomains([[1, 2], [3], [0], [5]], 5)
+  const expected = [
+    [new Set([0, 2, 3]), new Set([0, 3, 4]), new Set([1, 3, 4])],
+    [new Set([0, 1, 2]), new Set([1, 2, 3]), new Set([2, 3, 4])],
+    [new Set()],
+    [new Set([0, 1, 2, 3, 4])],
+  ]
+  expect(actual).toEqual(expected)
+})
+
+describe('finding constraints', () => {
+  test('when no constraint exists should return an empty Set', () => {
+    const domain = generateDomain([1], 5)
+    const actual = findConstraint(domain)
+    const expected = new Set()
+    expect(actual).toEqual(expected)
   })
 
-  test('no values', () => {
-    expect(generateDomain([1], 3)).toEqual([
-      new Set([0]),
-      new Set([1]),
-      new Set([2]),
-    ])
+  test('when a domain has a length of one should return the remaining Set', () => {
+    const domain = generateDomain([2, 2], 5)
+    const actual = findConstraint(domain)
+    const expected = new Set([0, 1, 3, 4])
+    expect(actual).toEqual(expected)
   })
 
-  test('single value without space', () => {
-    expect(generateDomain([3], 3)).toEqual([new Set([0, 1, 2])])
+  test('when a single constraint exists should return the constraint', () => {
+    const domain = generateDomain([2], 3)
+    const actual = findConstraint(domain)
+    const expected = new Set([1])
+    expect(actual).toEqual(expected)
   })
 
-  test('single value with space', () => {
-    expect(generateDomain([3], 4)).toEqual([
-      new Set([0, 1, 2]),
-      new Set([1, 2, 3]),
-    ])
-  })
-
-  test('multiple values without space', () => {
-    expect(generateDomain([2, 1], 4)).toEqual([new Set([0, 1, 3])])
-  })
-
-  test('multiple values with space', () => {
-    expect(generateDomain([2, 1], 5)).toEqual([
-      new Set([0, 1, 3]),
-      new Set([0, 1, 4]),
-      new Set([1, 2, 4]),
-    ])
-  })
-
-  test('multiple values with space', () => {
-    expect(generateDomain([2, 1], 5)).toEqual([
-      new Set([0, 1, 3]),
-      new Set([0, 1, 4]),
-      new Set([1, 2, 4]),
-    ])
+  test('when multiple constraint exists should return all constraints', () => {
+    const domain = generateDomain([3], 4)
+    const actual = findConstraint(domain)
+    const expected = new Set([1, 2])
+    expect(actual).toEqual(expected)
   })
 })
