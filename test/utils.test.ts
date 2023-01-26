8b23c4c54d9a78aa37accb05072b1e890e013ebf
drewrh <drewrh04@gmail.com>
2023-01-26 17:32:05 -0500

    Updated unit tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -4,12 +4,21 @@ import { generateAllDomains, generateDomain } from '@/util/domain'
 test('domains generated correctly', () => {
   const actual = generateAllDomains([[1, 2], [3], [0], [5], [5]], 5)
   const expected = [
-    [new Set([0, 2, 3]), new Set([0, 3, 4]), new Set([1, 3, 4])],
-    [new Set([0, 1, 2]), new Set([1, 2, 3]), new Set([2, 3, 4])],
+    [
+      new Set([1, 3, 4, -2, -5]),
+      new Set([1, 4, 5, -2, -3]),
+      new Set([2, 4, 5, -1, -3]),
+    ],
+    [
+      new Set([1, 2, 3, -4, -5]),
+      new Set([2, 3, 4, -1, -5]),
+      new Set([3, 4, 5, -1, -2]),
+    ],
     [new Set()],
-    [new Set([0, 1, 2, 3, 4])],
-    [new Set([0, 1, 2, 3, 4])],
+    [new Set([1, 2, 3, 4, 5])],
+    [new Set([1, 2, 3, 4, 5])],
   ]
+
   expect(actual).toEqual(expected)
 })
 
@@ -24,21 +33,21 @@ describe('finding constraints', () => {
   test('when a domain has a length of one should return the remaining Set', () => {
     const domain = generateDomain([2, 2], 5)
     const actual = findConstraint(domain)
-    const expected = new Set([0, 1, 3, 4])
+    const expected = new Set([1, 2, 4, 5, -3])
     expect(actual).toEqual(expected)
   })
 
   test('when a single constraint exists should return the constraint', () => {
     const domain = generateDomain([2], 3)
     const actual = findConstraint(domain)
-    const expected = new Set([1])
+    const expected = new Set([2])
     expect(actual).toEqual(expected)
   })
 
   test('when multiple constraint exists should return all constraints', () => {
     const domain = generateDomain([3], 4)
     const actual = findConstraint(domain)
-    const expected = new Set([1, 2])
+    const expected = new Set([2, 3])
     expect(actual).toEqual(expected)
   })
 })
