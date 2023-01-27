486c77ef88ebe28c4aa5ba242569962154b5ddda
drewrh <drewrh04@gmail.com>
2023-01-26 20:51:19 -0500

    Refactoring

--- src/solve.ts
+++ src/solve.ts
@@ -1,4 +1,4 @@
-import { applyConstraint, searchForConstraints } from './util/constraint'
+import { reduceSiblingDomain } from './util/constraint'
 import { generateAllDomains } from './util/domain'
 
 export function solve(
@@ -25,44 +25,20 @@ export function solve(
     solvedColumns.size !== columns.length
   ) {
     rowDomains.forEach((domain, index) => {
-      if (!solvedRows.has(index)) {
-        const constraints = searchForConstraints(domain)
-        constraints.forEach((knownValue) => {
-          columnDomains[Math.abs(knownValue) - 1] =
-            knownValue > 0
-              ? applyConstraint(index + 1, columnDomains[knownValue - 1])
-              : applyConstraint(
-                  -(index + 1),
-                  columnDomains[Math.abs(knownValue) - 1]
-                )
-        })
-      }
+      reduceSiblingDomain(domain, index, columnDomains)
       if (domain.length === 1) {
         solvedRows.add(index)
       }
     })
     columnDomains.forEach((domain, index) => {
-      if (!solvedColumns.has(index)) {
-        const constraints = searchForConstraints(domain)
-        constraints.forEach((knownValue) => {
-          rowDomains[Math.abs(knownValue) - 1] =
-            knownValue > 0
-              ? applyConstraint(index + 1, rowDomains[knownValue - 1])
-              : applyConstraint(
-                  -(index + 1),
-                  rowDomains[Math.abs(knownValue) - 1]
-                )
-        })
-      }
+      reduceSiblingDomain(domain, index, rowDomains)
       if (domain.length === 1) {
         solvedColumns.add(index)
       }
     })
   }
-  const solveEnd = performance.now()
-  console.log(solvedRows)
-  console.log(solvedColumns)
 
+  const solveEnd = performance.now()
   console.log(`Time to solve: ${solveEnd - solveStart} ms`)
 
   return []
